b12a3162a088a39711a32e8e63e32aa25d0922b2
martin <lenaerts.martin61@gmail.com>
2022-07-13 14:05:22 +0200

    create api

---
+++ Controllers/WeatherController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace marine_tp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("weather")]
+        public string Weather()
+        {
+            return "this route '/weather' return this";   
+        }
+
+        [HttpGet]
+        [Route("sun")]
+        public string Sun()
+        {
+            return "this route '/sun' return this";
+        }
+    }
+}
